504de94bace6685fdf2b2f8c07738ebe30af06fa
jaywomack <womackjw@hotmail.com>
2022-06-12 14:07:03 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+# To-Do App
+
+
+# Description: The classic To-Do application where a user can write down all the things they want to accomplish.
+
+# User Stories
+### Users can log in on the command line as a user or as an admin.
+
+
+### for a user the current options are:
+### The current todos are displayed
+### The user is prompted to open menu or quit
+### The user can type menu to see the menu
+### The user can type quit to exit the program
+### From the menu the user can:
+### - add a new todo
+### - view all the todos
+### - view all the completed todos
+### - view all the uncompleted todos
+### - mark a todo as complete
+### - Lookup todo by name and edit a todo
+### - delete a todo
+
+### If the user is an admin the current options are:
+
+### - view all the todos by a specific user
+### - view all the completed todos by a specific user
+### - view all the uncompleted todos by a specific user
+### - view all the todos by a specific date
+### - view all the completed todos by a specific date
+### - view all the uncompleted todos by a specific date
+### - create a performance report from a specific user
+
+
+# Bonus features
+# User can edit a to-do
+# User can see a list with all the completed to-do’s
+# User can see a list with all the active to-do’s
+# User can see the date when he created the to-do
+# When closing the browser window the to-do’s will be stored and when the User returns, the data will be retrieved
+# Packages:
+# Rich for colorful terminals
+# Tables
+# Users
+# Fields:
+# Id primary key auto incrementing
+# Username varchar255
+# Password varchar255 
+# Todos
+# Fields:
+# Id primary key auto incrementing
+# Username varchar255
+# Password varchar255
+# Category? Todo or In Process or Complete 
+ 
+
