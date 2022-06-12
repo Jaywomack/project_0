1fb55e0f731da7c9e2c49b387b1c081b2098ab39
jaywomack <womackjw@hotmail.com>
2022-06-12 15:14:26 -0700

    Creating project plan

--- main.py
+++ main.py
@@ -1,56 +1,82 @@
 # To-Do App
 
+### 1. Plan
+### 2. Design
+### 3. Code
+### 4. Test
+### 5. Deploy
 
 # Description: The classic To-Do application where a user can write down all the things they want to accomplish.
 
-# User Stories
-### Users can log in on the command line as a user or as an admin.
+### User Stories
+#### Users can log in on the command line as a user or as an admin.
 
 
-### for a user the current options are:
-### The current todos are displayed
-### The user is prompted to open menu or quit
-### The user can type menu to see the menu
-### The user can type quit to exit the program
-### From the menu the user can:
-### - add a new todo
-### - view all the todos
-### - view all the completed todos
-### - view all the uncompleted todos
-### - mark a todo as complete
-### - Lookup todo by name and edit a todo
-### - delete a todo
+#### for a user the current options are:
+#### The current todos are displayed
+#### The user is prompted to open menu or quit
+#### The user can type menu to see the menu
+#### The user can type quit to exit the program
+#### From the menu the user can:
+#### - add a new todo
+#### - view all the todos
+#### - view all the completed todos
+#### - view all the uncompleted todos to a certain date
+#### - view all the uncompleted todos
+#### - mark a todo as complete
+#### - Lookup todo by name and edit a todo
+#### - delete a todo
 
-### If the user is an admin the current options are:
+#### If the user is an admin the current options are:
 
-### - view all the todos by a specific user
-### - view all the completed todos by a specific user
-### - view all the uncompleted todos by a specific user
-### - view all the todos by a specific date
-### - view all the completed todos by a specific date
-### - view all the uncompleted todos by a specific date
-### - create a performance report from a specific user
+#### - view all the todos by a specific user
+#### - view all the completed todos by a specific user
+#### - view all the uncompleted todos by a specific user
+#### - view all the todos by a specific date
+#### - view all the completed todos by a specific date
+#### - view all the uncompleted todos by a specific date
+#### - create a performance report from a specific user
+
+# Tech Stack
+        #### Python 3.9.13
+        #### Vs Code 1.68.0
+        #### MySQL 8.0.29
 
 
-# Bonus features
-# User can edit a to-do
-# User can see a list with all the completed to-do’s
-# User can see a list with all the active to-do’s
-# User can see the date when he created the to-do
-# When closing the browser window the to-do’s will be stored and when the User returns, the data will be retrieved
 # Packages:
-# Rich for colorful terminals
+        #### Rich for colorful terminals
+        #### sql-connector-python for database connection
+        #### datetime for date and time
+        #### argparse for command line arguments
+        #### sys for system functions
+        #### os for operating system functions
+        #### re for regular expressions
+
+
 # Tables
-# Users
-# Fields:
-# Id primary key auto incrementing
-# Username varchar255
-# Password varchar255 
-# Todos
-# Fields:
-# Id primary key auto incrementing
-# Username varchar255
-# Password varchar255
-# Category? Todo or In Process or Complete 
-2 + 2
+
+        ## Users
+                ### Fields:
+                        #### Id primary key auto incrementing
+                        #### Username varchar255
+                        #### Password stored as hash 
+                        #### Admin boolean
+                        #### Created_at datetime
+                        #### Updated_at datetime
+                        #### Deleted_at datetime
+
+
+
+        # Todos
+                ### Fields:
+                        #### Id 
+                        #### Category? Todo or In Process or Complete
+                        #### Username foreign key to users table
+                        #### Description varchar255
+                        #### Date varchar255
+                        #### Time varchar255
+                        #### Complete boolean
+                        #### CompleteDate varchar255
+                        #### CompleteTime varchar255
+
 
